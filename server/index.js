890ef053e359fb9f42ce2c8b91c0874095bc6a76
webermg <matthew.gerald.weber@gmail.com>
2020-10-07 15:33:34 -0700

    path change for static dir

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Routes
 // ===========================================================
